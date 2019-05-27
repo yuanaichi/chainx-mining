a7558e003740be4d56937bf22574a3089dea7183
yuanchao <blue5tar@163.com>
2019-05-28 00:35:46 +0800

    dist

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-yarn build && git checkout dist && cp -rf dist/* . && rm -rf dist && echo "dist copied.\n" && git add . && git commit -m "dist" && git push origin dist && git checkout master
-
-echo  "build ok!"
-
--- src/services/api.js
+++
@@ -1,6 +0,0 @@
-import request from '../utils/request';
-import { stringify } from 'qs';
-
-export async function queryXXX() {
-  return request('/api/xxx');
-}
